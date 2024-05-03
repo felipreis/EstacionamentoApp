ccc765dc919cedf4b7c0087b372c266f1b530db3
Felipe Reis <felipreisdaniel@gmail.com>
2024-05-03 12:58:23 -0300

    iniciando app estacionamento

---
+++ EstacionamentoApp/Estacionamento/src/main/java/com/estacionamento/model/App.java
@@ -0,0 +1,13 @@
+package com.estacionamento.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
